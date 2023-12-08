b24e67152879ff84e1e18e26bd4642a7322acbe3
LCB <dhtkfkdh2073@naver.com>
2023-12-08 17:41:04 +0900

    자바 1일차 : 테스트

---
+++ Java_lcb/src/day01/TestEx1.java
@@ -0,0 +1,22 @@
+package day01;
+
+public class TestEx1 {
+	
+	//1일차 테스트
+		public static void main(String[] args) {
+			//1. 아래 코드에서 잘못된 부분을 찾아 수정하세요.
+			//char ch1 = "A";
+			char ch1 = 'A';
+			System.out.println(ch1);
+			
+			//2. 국어, 영어, 수학 성적을 입력하여 평균을 구하려 한다. 이 때 필요한
+			//변수들을 선언하세요.
+			//정수로 할 것인가? 실수로 할 것인가?
+			int kor1, eng1, math1, sum1;
+			double kor2, eng2, math2, sum2;
+			double avg;
+	
+
+	}
+
+}
