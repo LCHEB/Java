c54532666c906b076efee4030f18265844b4fe6a
LCB <dhtkfkdh2073@naver.com>
2023-12-08 14:24:51 +0900

    자바 1일차 : 샘플 코드 및 설명

---
+++ Java_lcb/src/day01/HelloWorld.java
@@ -0,0 +1,19 @@
+package day01;
+
+//한줄 주석
+/*주석
+ * 여러줄 주석
+ * 메소드를 설명할 때 사용주석
+ * 	@param args
+ * 
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		//sysout 콘솔 출력할 때 사용
+		System.out.println("Hello World!");
+		
+	}
+
+}
+
