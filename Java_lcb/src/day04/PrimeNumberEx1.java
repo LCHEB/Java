baae385953981015a7ec10a3b8b331e0019fa1aa
LCB <dhtkfkdh2073@naver.com>
2023-12-13 12:47:10 +0900

    자바 4일차 : 이중반복문 소수 예제

---
+++ Java_lcb/src/day04/PrimeNumberEx1.java
@@ -0,0 +1,27 @@
+package day04;
+
+public class PrimeNumberEx1 {
+
+	public static void main(String[] args) {
+		/* 2부터 100사이의 모든 소수를 출력하는 코드를 작성하세요.
+		 * 소수 판별 예제 이용
+		 * 반복회수	: num는 2부터 100까지 1씩 증가
+		 * 규칙성 	: num가 소수이면 num를 출력
+		 */
+		
+		
+		for(int num = 2; num <= 100; num++) {
+			int count = 0, i;
+			for(i = 1; i <= num ; i++) {
+				if(num % i == 0) {
+					count++;
+				} 
+			}
+			if(count == 2) {
+				System.out.print(num + " ");
+			}
+		}
+		
+	}
+
+}
