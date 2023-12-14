f2f20d3269b08f71370c4e400ff874b441193d91
LCB <dhtkfkdh2073@naver.com>
2023-12-14 16:19:59 +0900

    자바 4일차 : 업다운 게임

--- Java_lcb/src/day04/UpDownGame.java
+++ Java_lcb/src/day04/UpDownGame.java
@@ -65,7 +65,7 @@ public class UpDownGame {
 			menu = scan.nextInt();
 		
 		if(menu == 1) {
-			int r = (int)(Math.random() * (max-min+1) + min);
+			int r = (int)(Math.random() * (max - min + 1) + min);
 			System.out.println(r);
 			count = 0;
 			for( ; num != r ; ) {
