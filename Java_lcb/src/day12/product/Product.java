b94bf6599eebfc9802b33d20426ade51735ad737
LCB <dhtkfkdh2073@naver.com>
2023-12-26 17:52:02 +0900

    자바 12일차 : 제품 관리 프로그램 - 추상 클래스, 상속 등 활용

--- Java_lcb/src/day12/product/Product.java
+++ Java_lcb/src/day12/product/Product.java
@@ -1,5 +1,13 @@
 package day12.product;
 
-public class Product {
+public abstract class Product {
+	String brand, code, name;
 
+	public Product(String brand, String code, String name) {
+		this.brand = brand;
+		this.code = code;
+		this.name = name;
+	}
+	
+	public abstract void print();
 }
--- Java_lcb/src/day12/product/ProductMain.java
+++ Java_lcb/src/day12/product/ProductMain.java
@@ -35,16 +35,34 @@ public class ProductMain {
 	 * 제품명 : 에어컨 Q9000
 	 * 냉방면적(m2) : 
 	 */
+	
+	static Product [] list = new Product[30];
+	static int count = 0;
+	
 	public static void main(String[] args) {
+		
 		int menu;
 		do {
-			printmenu();
+			//메뉴 출력
+			printMenu();
+			//메뉴 선택
 			menu = scan.nextInt();
-			runmenu(menu);
+			//기능 실행
+			runMenu(menu);
 		}while(menu != 3);
-	}
 
-	private static void runmenu(int menu) {
+	}
+	
+	public static void printMenu() {
+		System.out.println("-------메뉴-------");
+		System.out.println("1. 제품 추가");
+		System.out.println("2. 제품 확인");
+		System.out.println("3. 종료");
+		System.out.println("----------------");
+		System.out.print("메뉴 선택 : ");
+	}
+	
+	public static void runMenu(int menu) {
 		switch(menu) {
 		case 1:
 			insertProduct();
@@ -53,50 +71,68 @@ public class ProductMain {
 			printProduct();
 			break;
 		case 3:
-			System.out.println("종료합니다.");
+			System.out.println("프로그램 종료!");
 			break;
 		default:
 			System.out.println("잘못된 메뉴입니다.");
 		}
 	}
 
-	private static void printProduct() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	private static void insertProduct() {
+	public static void insertProduct() {
+		//제품 목록 출력
 		printProductTitle();
-		int submenu = scan.nextInt();
-		runSubMenu(submenu);
-	}
-
-	private static void runSubMenu(int submenu) {
-		switch(submenu) {
-		case 1:
+		//공통 정보
+		int select = scan.nextInt();
+		System.out.print("브랜드 : ");
+		scan.nextLine();//위에서 입력한 엔터 처리
+		String brand = scan.nextLine();
+		System.out.print("제품명 : ");
+		String title = scan.nextLine();
+		System.out.print("제품코드: ");
+		String code = scan.next();
+		//제품마다 추가 정보 입력 후 인스턴스 생성 후 추가
+		switch(select) {
+		case 1://tv
+			System.out.print("화면크기: " );
+			int size = scan.nextInt();
+			list[count++] = new Tv(brand, code, title, size);
 			break;
-		case 2:
+		case 2://에어컨 
+			System.out.print("냉방면적: ");
+			double area = scan.nextDouble();
+			list[count++] = new Aircon(brand, code, title, area);
 			break;
-		case 3:
+		case 3://노트북
+			System.out.print("CPU : ");
+			double cpu = scan.nextDouble();
+			System.out.print("램   : " );
+			int ram = scan.nextInt();
+			list[count++] = new Laptop(brand, code, title, cpu, ram);
 			break;
 		default:
 			System.out.println("잘못된 제품입니다.");
+			return;
 		}
+		System.out.println("등록이 완료됐습니다.");
 	}
-
-	private static void printProductTitle() {
-		System.out.println("추가할 제품");
-		System.out.println("1. Tv");
+	
+	public static void printProductTitle() {
+		System.out.println("제품 목록");
+		System.out.println("1. TV");
 		System.out.println("2. 에어컨");
 		System.out.println("3. 노트북");
-		System.out.println("제품 선택 : ");
+		System.out.print("제품 선택 : ");
 	}
 
-	private static void printmenu() {
-		System.out.println("1. 제품 추가");
-		System.out.println("2. 제품 확인");
-		System.out.println("3. 종료");
-		System.out.println("메뉴 선택 : ");
+	public static void printProduct() {
+		if(count == 0) {
+			System.out.println("등록된 제품이 없습니다.");
+			return;
+		}
+		
+		for(int i = 0; i<count; i++) {
+			list[i].print();
+		}
 	}
 	
 }
