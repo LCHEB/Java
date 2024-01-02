738e5727e04075a7f12a22ca37bd693e090d11e2
LCB <dhtkfkdh2073@naver.com>
2024-01-02 19:44:48 +0900

    팀과제 : 영어단어장

--- Java_lcb/src/home01/WordBookMain.java
+++ Java_lcb/src/home01/WordBookMain.java
@@ -4,7 +4,7 @@ package home01;
 public class WordBookMain {
 
 	public static void main(String[] args) {
-		WordBookProgram2 wp = new WordBookProgram2();
+		WordBookProgram wp = new WordBookProgram();
 		wp.run();
 	}
 	
--- Java_lcb/src/home01/WordBookProgram.java
+++ Java_lcb/src/home01/WordBookProgram.java
@@ -1,142 +1,121 @@
 package home01;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.InputMismatchException;
-import java.util.Map;
 import java.util.Scanner;
 
-
 public class WordBookProgram implements WordBook {
-	static Scanner scan = new Scanner(System.in);
-	static HashMap<String, String> wordbook = new HashMap<String, String>();
+
+	private static Scanner scan = new Scanner(System.in);
+	private static HashMap<String, ArrayList<String>> map = new HashMap<String, ArrayList<String>>();
+
+	private static int indexOf(String mean) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
 	@Override
 	public void run() {
 		int menu = 0;
+		//반복
 		do {
 			//메뉴 출력
 			printMenu();
 			try {
-			//메뉴 입력
+				//메뉴 선택
 				menu = scan.nextInt();
-			//메뉴 실행
-			runMenu(menu);
+				//메뉼 실행
+				runMenu(menu);
 			}catch(InputMismatchException e) {
 				System.out.println("잘못된 메뉴입니다.");
 				scan.nextLine();
 			}
-		}while(menu != 4);
+		}while(menu != 8);
+		
 	}
-	
 
 	@Override
 	public void printMenu() {
-		System.out.println("-----------메뉴---------");
-		System.out.println("1. 단어 관리");
-		System.out.println("2. 뜻 관리");
-		System.out.println("3. 단어 조회");
-		System.out.println("4. 종료");
-		System.out.println("-----------------------");
+		System.out.println("메뉴");
+		System.out.println("1.단어 추가");
+		System.out.println("2.단어 수정");
+		System.out.println("3.단어 삭제");
+		System.out.println("4.뜻 추가");
+		System.out.println("5.뜻 수정");
+		System.out.println("6.뜻 삭제");
+		System.out.println("7.조회");
+		System.out.println("8.종료");
 		System.out.print("메뉴 선택 : ");
+		
 	}
-	
+
 	@Override
 	public void runMenu(int menu) {
 		switch(menu) {
 		case 1:
-			wordManager();
+			//단어 추가
+			ArrayList<String> list = new ArrayList<String>();
+			System.out.print("추가할 단어 : ");
+			String word = scan.next();
+			if(map.containsKey(word)){
+				System.out.println("이미 있는 단어");
+			}else {
+				map.put(word, list);
+			}
 			break;
 		case 2:
-			meanManager();
+			//단어 수정
 			break;
 		case 3:
-			printManager();
+			//단어 삭제
 			break;
 		case 4:
-			System.out.println("프로그램을 종료합니다.");
+			//뜻 추가
+			System.out.print("뜻을 추가할 단어 : ");
+			word = scan.next();
+			if(map.containsKey(word)) {
+				System.out.print("추가할 뜻 : ");
+				String mean = scan.next();
+				map.get(word).add(mean);
+			}else {
+				System.out.println("없는 단어");
+			}
 			break;
-		default:
-			throw new InputMismatchException();
-		}
-
-	}
-
-	public void printManager() {
-		System.out.print("조회할 단어 : ");
-		String word = scan.next();
-		if(wordbook.containsKey(word) == true) {
-			System.out.println(wordbook.get(word));
-		}else {
-			System.out.println("등록되지 않은 단어입니다.");
-		}
-	}
-
-
-	public void meanManager() {
-		// TODO Auto-generated method stub
-		
-	}
-
-
-	public void wordManager() {
-		//단어 관리 메뉴를 출력
-			printWordMenu();
-			//단어 관리 메뉴를 선택
-			int menu = scan.nextInt();
-			//단어 관리 메뉴를 실행
-			runWordMenu(menu);
-		
-	}
-
-
-	public void runWordMenu(int menu) {
-		switch(menu) {
-		case 1:
-			insertWord();
+		case 5:
+			//뜻 수정
+			System.out.println("수정할 단어 : ");
+			word = scan.next();
+			if(map.containsKey(word)) {
+				System.out.print("수정할 뜻 : ");
+				String mean = scan.next();
+				if(map.get(word).indexOf(mean) != -1) {
+					System.out.println(mean + " 수정할 뜻 : ");
+					String mean2 = scan.next();
+					map.get(word).remove(mean);
+					map.get(word).add(mean2);
+					System.out.println("수정이 완료됐습니다.");
+				}
+			}else {
+				System.out.println("없는 단어");
+			}
 			break;
-		case 2:
-			updateWord();
+		case 6:
+			//뜻 삭제
 			break;
-		case 3:
-			//deleteWord();
+		case 7:
+			//조회
+			System.out.println(map);
+			break;
+		case 8:
+			//종료
+			System.out.println("종료");
 			break;
 		default:
 			throw new InputMismatchException();
 		}
 		
 	}
-
-
-	private void updateWord() {
-		System.out.print("단어 입력 : ");
-		String word = scan.next();
-		
-	}
-
-
-	public void insertWord() {
-		System.out.println("단어 입력 : ");
-		String word = scan.next();
-		if(wordbook.containsKey(word) == true) {
-			System.out.println("이미 등록된 단어입니다.");
-		}else {
-			System.out.print("뜻 입력 : ");
-			String mean = scan.next();
-			wordbook.put(word, mean);
-			
-		}
-	}
-
-
-	public void printWordMenu() {
-		System.out.println("----------단어 관리--------");
-		System.out.println("1. 단어 추가");
-		System.out.println("2. 단어 수정");
-		System.out.println("3. 단어 삭제");
-		System.out.println("-----------------------");
-		System.out.print("메뉴 선택 : ");
-		
-	}
-
 
 
 
