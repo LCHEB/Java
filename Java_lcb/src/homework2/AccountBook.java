779ac5048a42bb9e8d1e3604053893912e729e82
LCB <dhtkfkdh2073@naver.com>
2024-01-07 15:48:37 +0900

    팀과제2 : 가계부

--- Java_lcb/src/homework2/AccountBook.java
+++ Java_lcb/src/homework2/AccountBook.java
@@ -1,15 +1,38 @@
 package homework2;
 
 import java.util.Date;
+import java.util.Objects;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
-@AllArgsConstructor
 public class AccountBook {
-	Date date;
-	String use;
-	int money,inmoney,outmoney;
-	int totalmoney, totalInMoney, totalOutMoney;
+	int num; //번호
+	String date; //날짜
+	int money,inMon,outMon; //돈, 수입, 지출
+	String use; //내역
+	int totalmon, totalInMon, totalOutMon; //총 잔액, 총 수입, 총 지출
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		AccountBook other = (AccountBook) obj;
+		return Objects.equals(date, other.date) && inMon == other.inMon && money == other.money && num == other.num
+				&& outMon == other.outMon && Objects.equals(use, other.use);
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(date, inMon, money, num, outMon, use);
+	}
+	
+	
+	
+	
+	
 }
--- Java_lcb/src/homework2/AccountBookProgram.java
+++ Java_lcb/src/homework2/AccountBookProgram.java
@@ -1,6 +1,6 @@
 package homework2;
 
-import java.util.Date;
+import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -8,9 +8,9 @@ import program.Program;
 
 public class AccountBookProgram implements Program {
 	private Scanner scan = new Scanner(System.in);
-	private Date date = new Date();
+	private static AccountBook ab = new AccountBook();
+	private static ArrayList<AccountBook> acc = new ArrayList<>();
 	private final int EXIT = 4;
-	private int num = 0;
 	@Override
 	public void run() {
 		int menu = 0;
@@ -34,7 +34,8 @@ public class AccountBookProgram implements Program {
 		System.out.println("------메뉴------");
 		System.out.println("1. 가계부 작성");
 		System.out.println("2. 가계부 조회");
-		System.out.println("3. 종료");
+		System.out.println("3. 가계부 검색");
+		System.out.println("4. 종료");
 		System.out.println("---------------");
 		System.out.print("메뉴 선택 : ");
 
@@ -44,12 +45,15 @@ public class AccountBookProgram implements Program {
 	public void runMenu(int menu) {
 		switch(menu) {
 		case 1:
-			insertAccountBook();
+			insert();
 			break;
 		case 2:
-			searchAccountBook();
+			look();
 			break;
 		case 3:
+			search();
+			break;
+		case 4:
 			System.out.println("종료합니다.");
 			break;
 		default:
@@ -58,26 +62,194 @@ public class AccountBookProgram implements Program {
 
 	}
 
-	private void searchAccountBook() {
+	private void search() {
+		printSearch();
+		//단어 관리 메뉴를 선택
+		int menu = scan.nextInt();
+		//단어 관리 메뉴를 실행
+		runSearch(menu);
 		
 	}
 
-	private void insertAccountBook() {
-		System.out.println("날짜 : ");
-		scan.nextLine();
-		String date = scan.nextLine();
-		System.out.println("사용처 : ");
-		String use = scan.next();
-		System.out.println("금액 : ");
-		int money = scan.nextInt();
-		System.out.println("수입(1) / 지출(2) : ");
-		int num = scan.nextInt();
-		if(num == 1) {
-			
-		}else if(num == 2) {
-			
-		}else {
-			
+	private void printSearch() {
+		System.out.println("1.날짜 검색");
+		System.out.println("2. 이전으로");
+		System.out.println("---------------");
+		System.out.print("메뉴 선택 : ");
+	}
+
+	private void runSearch(int menu) {
+		switch(menu) {
+		case 1:
+			System.out.print("날짜 입력: ");
+	         String sdate = scan.next();
+	         int j = 0;
+	         if (j == acc.size()) {
+	            System.out.println("검색 결과 없음.");
+	         
+	         for (j = 0; j < acc.size(); j++) {
+	            ab = acc.get(j);
+	            if (sdate.equals(ab.getDate())) {
+	               int Insum = ab.inMon;
+	               int Outsum = ab.outMon;
+	               int sum = 0;
+	               sum = sum + (Insum + -Outsum);
+	               System.out.println("번호: " + ab.num);
+	               System.out.println("날짜: " + ab.date);
+	               System.out.println("수입: " + Insum);
+	               System.out.println("지출: " + -Outsum);
+	               System.out.println("내역: " + ab.use);
+	               System.out.println("합계: " + sum);
+	               System.out.println();
+	            }
+	        }
+        } 
+			break;
+		case 2:
+			System.out.println("이전화면으로");
+			return;
+		default:
+			System.out.println("잘못된 메뉴입니다.");
+		}
+	}
+
+	private void look() {
+		printLook();
+		//단어 관리 메뉴를 선택
+		int menu = scan.nextInt();
+		//단어 관리 메뉴를 실행
+		runLook(menu);
+	}
+
+	private void printLook() {
+		System.out.println("1. 전체 출력");
+		System.out.println("2. 수입 출력");
+		System.out.println("3. 지출 출력");
+		System.out.println("4. 이전으로");
+		System.out.println("---------------");
+		System.out.print("메뉴 선택 : ");
+		
+	}
+
+	private void runLook(int menu) {
+		switch(menu) {
+		case 1:
+			 int sum = 0;      
+	         int i = 0;
+	         if (i == acc.size()) {
+	            System.out.println("검색 결과 없음.");
+	         }
+	         for (i = 0; i < acc.size(); i++) {
+	            ab = acc.get(i);
+	            int Insum = ab.inMon;
+	            int Outsum = ab.outMon;            
+	            sum = sum + (Insum + -Outsum);
+	            System.out.println("번호: " + ab.num);
+	            System.out.println("날짜: " + ab.date);
+	            System.out.println("수입: " + Insum);
+	            System.out.println("지출: " + -Outsum);
+	            System.out.println("내역: " + ab.use);
+	            System.out.println("합계: " + sum);
+	            System.out.println();
+	         }
+	         break;
+		case 2:
+			 int inall = 0;   
+	         i = 0;
+	         if (i == acc.size()) {
+	            System.out.println("검색 결과 없음.");
+	         }
+	         for (i = 0; i < acc.size(); i++) {
+	            ab = acc.get(i);            
+	            int Insum = ab.inMon;
+	            inall = (inall + Insum);            
+	            System.out.println("번호: " + ab.num);
+	            System.out.println("날짜: " + ab.date);
+	            System.out.println("수입: " + Insum);         
+	            System.out.println("내역: " + ab.use);
+	            System.out.println("합계: " + inall);   
+	            System.out.println();
+	         }
+			break;
+		case 3:
+			 int outAll = 0;   
+	         i = 0;
+	         if (i == acc.size()) {
+	            System.out.println("검색 결과 없음.");
+	         }
+	         for (i = 0; i < acc.size(); i++) {
+	            ab = acc.get(i);
+	            int Outsum = ab.outMon;            
+	            outAll = outAll + Outsum;
+	            System.out.println("번호: " + ab.num);
+	            System.out.println("날짜: " + ab.date);
+	            System.out.println("지출: " + -Outsum);
+	            System.out.println("내역: " + ab.use);
+	            System.out.println("합계: " + -outAll);
+	            System.out.println();
+	         }
+	         break;
+		case 4:
+			System.out.println("이전화면으로");
+			return;
+		default:
+			System.out.println("잘못된 메뉴입니다.");
+		}
+	}
+
+	private void insert() {
+		printInsert();
+		//단어 관리 메뉴를 선택
+		int menu = scan.nextInt();
+		//단어 관리 메뉴를 실행
+		runInsert(menu);
+		
+	}
+
+
+	private void printInsert() {
+		System.out.println("1. 수입 등록");
+		System.out.println("2. 지출 등록");
+		System.out.println("3. 이전으로");
+		System.out.println("---------------");
+		System.out.print("메뉴 선택 : ");
+	}
+	
+	private void runInsert(int menu) {
+		switch(menu) {
+		case 1:
+			ab = new AccountBook();
+			System.out.print("번호 : ");
+			ab.setNum(scan.nextInt());
+			System.out.print("날짜 : ");
+			ab.setDate(scan.next());
+			System.out.print("수입 : ");
+			ab.setInMon(scan.nextInt());
+			ab.setOutMon(0);
+			System.out.print("내역 : ");
+			ab.setUse(scan.next());
+			acc.add(ab);
+			System.out.println("수입을 등록했습니다.");
+			break;
+		case 2:
+			ab = new AccountBook();
+			System.out.print("번호 : ");
+			ab.setNum(scan.nextInt());
+			System.out.print("날짜 : ");
+			ab.setDate(scan.next());
+			System.out.print("지출 : ");
+			ab.setOutMon(scan.nextInt());
+			ab.setInMon(0);
+			System.out.print("내역 : ");
+			ab.setUse(scan.next());
+			acc.add(ab);
+			System.out.println("지출을 등록했습니다.");
+			break;
+		case 3:
+			System.out.println("이전화면으로");
+			return;
+		default:
+			System.out.println("잘못된 메뉴입니다.");
 		}
 	}
 
