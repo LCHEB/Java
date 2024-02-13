66488c44daac620dbe5815f9c494240b8ac56ed9
LCB <dhtkfkdh2073@naver.com>
2024-02-13 16:21:23 +0900

    DB 7일차 : 윈도우 함수 예제

---
+++ db/sql/13_윈도우 함수 예제.sql
@@ -0,0 +1,18 @@
+# 쇼핑몰을 이용한 윈도우 함수 예제
+
+# 제품을 가격순으로 정렬 row_number
+select row_number() over(order by pr_pride desc) as 순서, product.* from product;
+
+# 제품을 가격순으로 정렬 rank
+select rank() over(order by pr_pride desc) as 금액순, product.* from product;
+
+# 제품을 가격순으로 정렬 dense_rank
+select dense_rank() over(order by pr_pride desc) as 금액순, product.* from product;
+
+# 제품을 비싼 제품, 싼 제품으로 정렬 ntile
+select ntile(2) over(order by pr_pride desc) as 그룹, product.* from product;
+
+# 가장 비싼 제품들을 조회하는 쿼리 dense_rank
+select * from (
+	select dense_rank() over(order by pr_pride desc) as 'pr_rank', product.* from product
+) as ranked_product where pr_rank = 1;
