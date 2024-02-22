1d19cbef420009bbbf03ba682c290f9af8b3226b
LCB <dhtkfkdh2073@naver.com>
2024-02-22 16:50:46 +0900

    서블릿 1일차 : 로그인, 회원가입 기능 구현 완료

--- servlet/servlet_project1/src/main/java/kr/kh/app/controller/SignupServlet.java
+++ servlet/servlet_project1/src/main/java/kr/kh/app/controller/SignupServlet.java
@@ -1,17 +1,24 @@
 package kr.kh.app.controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import kr.kh.app.model.vo.MemberVO;
+import kr.kh.app.service.MemberService;
+import kr.kh.app.service.MemberServiceImp;
+
 @WebServlet("/signup")
 public class SignupServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
-    public SignupServlet() {
+	
+	private MemberService memberService = new MemberServiceImp();
+    
+	public SignupServlet() {
 
     }
 
@@ -21,12 +28,16 @@ public class SignupServlet extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String id = request.getParameter("id");
-		System.out.println("전송 받은 ID : " + id);
 		String pw = request.getParameter("pw");
-		System.out.println("전송 받은 PW : " + pw);
 		String email = request.getParameter("email");
-		System.out.println("전송 받은 EMAIL : " + email);
-		doGet(request, response);
+		if(memberService.signup(new MemberVO(id, pw, email))) {
+			//회원가입에 성공하면 메인 페이지로 이동
+			response.sendRedirect(request.getContextPath()+"/");
+		}
+		else {
+			//실패하면 회원가입 페이지 유지
+			doGet(request, response);
+		}
 	}
 
 }
