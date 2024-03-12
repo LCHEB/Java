4ad32ba1fc18cfce45ad67eaf0015c6f2d6d7b58
LCB <dhtkfkdh2073@naver.com>
2024-03-12 15:50:53 +0900

    스프링 1일차 : 롬북

---
+++ Spring/spring/src/main/java/kr/kh/spring/model/vo/MemberVO.java
@@ -0,0 +1,16 @@
+package kr.kh.spring.model.vo;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class MemberVO {
+	private String me_id;
+	private String me_pw;
+	private String me_ms_state;
+	private String me_email;
+	private String me_authority;
+	private String me_stop;
+	private String me_fail;
+}
